0f26f5f484fb76b12aefaaff800c3363d1ce7269
Alexandre Arnt <qtgzmanager@gmail.com>
2021-11-10 20:49:07 -0300

    BugFix in zoom in code

--- keybindings.c
+++ keybindings.c
@@ -189,21 +189,26 @@ commonCb_key_press_event(struct Window *window, GdkEvent *event, struct Client *
 	gdouble zoom = 0;
 	const gchar *duckUrl = "https://lite.duckduckgo.com";
 	const gchar *searxUrl = "https://searx.info";
-  gchar *statusLabel = NULL;
+	gchar *statusLabel = NULL;
 
-  if((((GdkEventKey *)event)->state & (GDK_CONTROL_MASK | GDK_SHIFT_MASK)) == (GDK_CONTROL_MASK | GDK_SHIFT_MASK))
-  {
-    if(browser != NULL)
-    {
-      switch(((GdkEventKey *)event)->keyval)
-      {
-      case GDK_KEY_ISO_Left_Tab:
-        goto_prev_tab(notebook);
-        return TRUE;
-      }
-    }
-  }
-  else if(((GdkEventKey *)event)->state & GDK_CONTROL_MASK)
+	if((((GdkEventKey *)event)->state & (GDK_CONTROL_MASK | GDK_SHIFT_MASK)) == (GDK_CONTROL_MASK | GDK_SHIFT_MASK))
+ 	{
+		if(browser != NULL)
+		{
+			switch(((GdkEventKey *)event)->keyval)
+			{
+			case GDK_KEY_ISO_Left_Tab:
+				goto_prev_tab(notebook);
+				return TRUE;
+			case GDK_KEY_plus:
+				zoom = webkit_web_view_get_zoom_level(WEBKIT_WEB_VIEW(browser->webView));
+				zoom += zoom * 0.1;
+				webkit_web_view_set_zoom_level(WEBKIT_WEB_VIEW(browser->webView), zoom);
+				return TRUE;
+      			}
+		}
+	}
+  	else if(((GdkEventKey *)event)->state & GDK_CONTROL_MASK)
 	{
 		if(browser != NULL)
 		{
@@ -218,11 +223,6 @@ commonCb_key_press_event(struct Window *window, GdkEvent *event, struct Client *
 			case GDK_KEY_0:
 				webkit_web_view_set_zoom_level(WEBKIT_WEB_VIEW(browser->webView), 1);
 				return TRUE;
-			case GDK_KEY_plus:
-				zoom = webkit_web_view_get_zoom_level(WEBKIT_WEB_VIEW(browser->webView));
-				zoom += zoom * 0.1;
-				webkit_web_view_set_zoom_level(WEBKIT_WEB_VIEW(browser->webView), zoom);
-				return TRUE;
 			case GDK_KEY_minus:
 				zoom = webkit_web_view_get_zoom_level(WEBKIT_WEB_VIEW(browser->webView));
 				zoom += zoom * -0.1;
@@ -231,15 +231,15 @@ commonCb_key_press_event(struct Window *window, GdkEvent *event, struct Client *
 			case GDK_KEY_Tab:
 				goto_next_tab(notebook);
 				return TRUE;
-      case GDK_KEY_c:
-        statusLabel = strdup(gtk_label_get_text(GTK_LABEL(browser->statuslabel)));
-        if (statusLabel != NULL)
-        {
-          /* Set clipboard text */
-          gtk_clipboard_set_text(gtk_widget_get_clipboard(GTK_WIDGET(browser->webView), GDK_SELECTION_CLIPBOARD), statusLabel, -1);
-        }
-        return TRUE;
-      case GDK_KEY_d:
+			case GDK_KEY_c:
+				statusLabel = strdup(gtk_label_get_text(GTK_LABEL(browser->statuslabel)));
+				if (statusLabel != NULL)
+				{
+					/* Set clipboard text */
+					gtk_clipboard_set_text(gtk_widget_get_clipboard(GTK_WIDGET(browser->webView), GDK_SELECTION_CLIPBOARD), statusLabel, -1);
+				}
+				return TRUE;
+			case GDK_KEY_d:
 				open_site_on_new_tab(window, duckUrl, false);
 				return TRUE;
 			case GDK_KEY_f:
